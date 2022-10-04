f696c246f4d85a1868da14de1125a2e9054addc2
José João Silva Nunes Alves <josejoaosilvanunesalves@10-45-49-149.0200.ifce.br.iosda.org>
2022-10-04 12:02:50 -0300

    add button

--- GitDay/GitDay/View.swift
+++ GitDay/GitDay/View.swift
@@ -1,16 +1,31 @@
-//
-//  View.swift
-//  GitDay
-//
-//  Created by José João Silva Nunes Alves on 04/10/22.
-//
-
 import UIKit
 
 class View: UIView {
 
+    private let button: UIButton = {
+        let button = UIButton()
+        button.setTitle("Title", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
     override func layoutSubviews() {
         super.layoutSubviews()
         backgroundColor = .blue
+        
+        configView()
+        setupConstraints()
+    }
+    
+    private func configView() {
+        addSubview(button)
+    }
+
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            button.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            button.heightAnchor.constraint(equalToConstant: 48)
+        ])
     }
 }
