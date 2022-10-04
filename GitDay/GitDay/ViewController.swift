1028c37ee5ed1fb8ac5f57b191eb1fa86504fa6f
José João Silva Nunes Alves <josejoaosilvanunesalves@10-45-49-149.0200.ifce.br.iosda.org>
2022-10-04 11:35:46 -0300

    add example project

---
+++ GitDay/GitDay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitDay
+//
+//  Created by José João Silva Nunes Alves on 04/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
