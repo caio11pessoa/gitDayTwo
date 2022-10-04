12a4a9d1bf629977ac6380d5e61af7ddc4de8875
José João Silva Nunes Alves <josejoaosilvanunesalves@10-45-49-149.0200.ifce.br.iosda.org>
2022-10-04 12:10:51 -0300

    add subtitle

--- GitDay/GitDay/View.swift
+++ GitDay/GitDay/View.swift
@@ -4,14 +4,25 @@ class View: UIView {
 
     private let button: UIButton = {
         let button = UIButton()
-        button.setTitle("Title", for: .normal)
+        button.setTitle("Title Button", for: .normal)
+        button.setTitleColor(.label, for: .normal)
+        button.backgroundColor = .systemGray
+        button.layer.cornerRadius = 8
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
 
+    private let subtitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "subtitle"
+        label.textColor = .label
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
     override func layoutSubviews() {
         super.layoutSubviews()
-        backgroundColor = .blue
+        backgroundColor = .systemBackground
         
         configView()
         setupConstraints()
@@ -19,6 +30,7 @@ class View: UIView {
     
     private func configView() {
         addSubview(button)
+        addSubview(subtitleLabel)
     }
 
     private func setupConstraints() {
@@ -26,6 +38,11 @@ class View: UIView {
             button.centerXAnchor.constraint(equalTo: self.centerXAnchor),
             button.centerYAnchor.constraint(equalTo: self.centerYAnchor),
             button.heightAnchor.constraint(equalToConstant: 48)
+        ])
+        
+        NSLayoutConstraint.activate([
+            subtitleLabel.topAnchor.constraint(equalTo: button.bottomAnchor, constant: 16),
+            subtitleLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor)
         ])
     }
 }
